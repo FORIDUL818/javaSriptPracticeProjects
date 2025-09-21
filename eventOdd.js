9bf4cd8d2cae02d18780cc52b3aa072fbe4c1843
Foridul islam <foridulislamdise880@gmail.com>
2025-09-21 19:44:36 +0600

    javaSript project Practice create namta and event odd system

---
+++ eventOdd.js
@@ -0,0 +1,17 @@
+let n=11;
+
+let data;
+if (n % 2 == 0) {
+    data=true
+}
+else{
+    data=false;
+}
+
+if (data) {
+    console.log(`your ${n}  data is event `)
+}
+else{
+    console.log(`your ${n}  data is odd`)
+
+}
---
+++ namta.js
@@ -0,0 +1,13 @@
+function Numta(value) {
+    
+    for (let i = 1; i < 10; i++) {
+        console.log(value + " * " + i + " = ", value * i )
+        
+    }
+
+}
+
+let num=10;
+Numta(num);
+
+
